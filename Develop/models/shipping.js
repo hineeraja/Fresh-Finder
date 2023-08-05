fcc7013ec7a15a24eecdbc0b56b70cf13a5fea5c
Vikram <vikram@Vikrams-MacBook-Pro.local>
2023-08-05 19:19:42 -0300

    added db_models, confgure file, schema.sql and other starter files updated

---
+++ Develop/models/shipping.js
@@ -0,0 +1,61 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('..config/connection');
+
+class Shipping extends Model { }
+
+Shipping.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+
+        },
+
+        full_name: {
+            type: DataTypes.STRING(150),
+            allowNull: false,
+
+        },
+
+        address: {
+            type: DataTypes.STRING(150),
+            allowNull: false,
+
+        },
+
+        city: {
+            type: DataTypes.STRING(150),
+            allowNull: false,
+
+        },
+
+        country: {
+            type: DataTypes.STRING(150),
+            allowNull: false,
+
+        },
+
+        postalcode: {
+            type: DataTypes.STRING(150),
+            allowNull: false,
+
+        },
+
+
+
+    },
+
+
+    {
+        sequelize,
+        freezeTableName: true,
+        modelName: 'category',
+
+    }
+);
+
+Shipping.belongsTo(User);
+
+module.exports = Category;
