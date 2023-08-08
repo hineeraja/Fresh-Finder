ca5769a01c166491dec7046a55668b604c730fe1
lexrayne <lexrayne@outlook.com>
2023-08-08 20:02:41 -0300

    Added category and subcategory routes

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
+const categoryRoutes = require('./categoryRoutes');
+const subcategoryRoutes = require('./subcategoryRoutes');
 
 router.use('/', homeRoutes);
+router.use('/categories', categoryRoutes);
+router.use('./subcategories', subcategoryRoutes);
 
 module.exports = router;
