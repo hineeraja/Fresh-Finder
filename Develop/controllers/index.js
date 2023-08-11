22271e53f41a0ff7da0e09ebe6c64849962b846a
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-08-11 19:58:24 -0300

    Added subcategory and product routes

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
 const categoryRoutes = require('./categoryRoutes');
+const subcategoryRoutes = require('./subcategoryRoutes');
+const productRoutes = require('./productRoutes');
 
 router.use('/categories', categoryRoutes);
+router.use('/subcategories', subcategoryRoutes);
+router.use('/products', productRoutes);
 router.use('/', homeRoutes);
 
 module.exports = router;
