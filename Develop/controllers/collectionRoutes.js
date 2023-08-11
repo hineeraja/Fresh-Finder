55d492479ed37198eccc96624ddc6bd4dd690d99
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-08-11 20:17:04 -0300

    Collection routes

--- Develop/controllers/collectionRoutes.js
+++ Develop/controllers/collectionRoutes.js
@@ -1,12 +1,26 @@
 const router = require('express').Router();
+const { col } = require('sequelize');
 const Collection = require('../models/collection');
 
-router.get('/collections/:id', async (req, res) => {
+router.get('/', async (req, res) => {
+    try {
+        const collectionData = await Collection.findAll();
+        res.status(200).json(collectionData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get('/:id', async (req, res) => {
     try {
         const collectionData = await Collection.findByPk(req.params.id);
-        const collection = collectionData.get({ plain: true });
+        //const collection = collectionData.get({ plain: true });
 
-        res.render('collection', collection);
+        if (!collectionData) {
+            res.status(404).json({ message: 'No collection found with this id' });
+            return;
+        }
+        res.status(200).json(collectionData);
     } catch (err) {
         res.status(500).json(err);
     }
