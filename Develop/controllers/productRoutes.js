ef0f254799ec237792b1a189a15a093e9ac45871
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-08-11 19:58:29 -0300

    Product routes

--- Develop/controllers/productRoutes.js
+++ Develop/controllers/productRoutes.js
@@ -1,12 +1,25 @@
 const router = require('express').Router();
 const Product = require('../models/product');
 
-router.get('/products/:id', async (req, res) => {
+router.get('/', async (req, res) => {
+    try {
+        const productData = await Product.findAll();
+        res.status(200).json(productData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
     try {
         const productData = await Product.findByPk(req.params.id);
-        const product = productData.get({ plain: true });
+        //const product = productData.get({ plain: true });
 
-        res.render('product', product);
+        if (!productData) {
+            res.status(404).json({ message: 'No product found with this id'});
+            return;
+        }
+        res.status(200).json(productData);
     } catch (err) {
         res.status(500).json(err);
     }
